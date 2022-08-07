894fd07af411b94b77a08bf486c4d9577e943d73
Sam0rai <yaronking@gmail.com>
2022-08-07 12:03:24 +0300

    Add files via upload

---
+++ Get-DownloadedFile-URL.ps1
@@ -0,0 +1,22 @@
+﻿$folder = C:\Users\JohnDoe\Downloads
+$resultsArray = @()
+$files = Get-ChildItem $folder -File
+
+foreach($file in $files) {
+    try {
+        $zoneIdentifier = Get-Content -Path $file.FullName -Stream Zone.Identifier
+        if($zoneIdentifier) {
+            $match = $zoneIdentifier -match "ReferrerUrl=(<Referr>\w+), "
+            $obj = New-Object PSObject -Property @{
+                FullName    = $file.FullName
+                ReferrerUrl = ($zoneIdentifier -match "ReferrerUrl=").Replace("ReferrerUrl=", "")
+                HostUrl     = ($zoneIdentifier -match "HostUrl=").Replace("HostUrl=", "")
+            }
+            $resultsArray += $obj
+        }
+    }
+    catch { }
+}
+
+$resultsArray | Select FullName, ReferrerUrl, HostUrl | ogv
+$resultsArray | Select FullName, ReferrerUrl, HostUrl | Export-Csv c:\temp\HostUrl.csv -NoTypeInformation -Encoding Unicode
